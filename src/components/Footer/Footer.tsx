a57a4d55d7999fbb2c09f616913c110c3910f827
RORN Makara <rornmakara89@gmail.com>
2025-05-27 10:15:28 +0700

    change telegram username

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import { Container } from './styles'
 import reactIcon from '../../assets/react-icon.svg'
 import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
 import instagramIcon from '../../assets/instagram.svg'
 
@@ -30,7 +29,7 @@ export function Footer() {
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://t.me/rmakaaraa"
+          href="https://t.me/rornmakaaraa"
           target="_blank"
           rel="noreferrer"
         >
